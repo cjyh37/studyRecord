dd69cca85c335f54b6bc03575e73fcf9cae75855
Ji Hoon Choi <admin@keldi.co.kr>
2024-05-03 17:27:15 +0900

    Update instances to 2 in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: 'lmskeldidev',
         script: './app.js',
-        instances: 0,
+        instances: 2,
         exec_mode: 'cluster',
         wait_ready: true,
         listen_timeout: 5000
